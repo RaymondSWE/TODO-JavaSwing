ddd0d64f5a915dfe95f33a281126aa7202cbdf15
nibarsito <nibar@hotmail.se>
2022-03-30 16:29:31 +0200

    added a git ignore file

--- OOP2022_a21rammo_assignment02/src/assignment2/ToDo.java
+++ OOP2022_a21rammo_assignment02/src/assignment2/ToDo.java
@@ -44,8 +44,8 @@ public class ToDo implements TaskListener,ActionListener {
 	
 
 	int total = 0, completed = 0;
-ArrayList<HomeTask> tasks=new ArrayList<HomeTask>();
-ArrayList<HomeTask> tasksAlphabetical=new ArrayList<HomeTask>();
+ArrayList<Object> tasks=new ArrayList<HomeTask>();
+ArrayList<Object> tasksAlphabetical=new ArrayList<HomeTask>();
 ArrayList<Object> completedTasks=new ArrayList<Object>();
 ArrayList<Object> unCompletedTasks=new ArrayList<Object>();
 
